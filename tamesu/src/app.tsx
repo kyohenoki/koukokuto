4543d4eacfdad6614688a591eafb8f55a721a46d
kyohei <kyohei103@outlook.jp>
2025-08-17 22:15:24 +0900

    mitame フォント

--- tamesu/src/app.tsx
+++ tamesu/src/app.tsx
@@ -4,7 +4,7 @@ import { Koukoku } from '@koukoku/index'
 export default function App() {
 	return (
 		<main className="antialiased p-5">
-			<h1 className="text-[1.15rem]">いいものの発見</h1>
+			<h1 className="text-[1.15rem]">いいものの発見 ･ koukoku</h1>
 			<Koukoku className="my-3" />
 		</main>
 	)
